8c60eee70fb9296aa5e5e3d793d323906956fb41
Tom Van Acker <van_acker_tom@me.com>
2022-02-16 11:56:50 +0100

    small changes

--- test/initial_tests.jl
+++ test/initial_tests.jl
@@ -13,8 +13,12 @@ T   = 1 / F
 Δt  = T / N 
 t   = 0.0:Δt:1.0-Δt
 
-# build the signal
+# build the signal `s`
 s   = 10.0 .* sin.((2 * pi * F) .* t)
 
-# get the info of the first harmonic
+# get the info of the harmonic `H`
 a⁰, ϕ⁰, a¹, ϕ¹, a², ϕ² = harmonic_state_estimator(s, K, N, H, F)
+
+# plot 
+plot(t, s)
+plot!(t, a⁰)

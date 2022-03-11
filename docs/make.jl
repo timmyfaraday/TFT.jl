978837a2704145c935c80ebd39ecabb89262a23d
Tom Van Acker <van_acker_tom@me.com>
2022-03-11 15:26:03 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@ using Documenter
 using TFT
 
 makedocs(
-    sitename = "TFT",
-    format = Documenter.HTML(),
-    modules = [TFT]
+    modules     = [TFT],
+    format      = Documenter.HTML(mathengine = Documenter.MathJax()),
+    sitename    = "TFT.jl",
+    authors     = "Tom Van Acker",
+    pages       = [ "Home"              => "index.md",
+                  ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+     repo = "github.com/timmyfaraday/TFT.jl.git"
+)

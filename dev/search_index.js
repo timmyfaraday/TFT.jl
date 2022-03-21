7211ca4858dea0c45e332c00c1defc6ff13c8171
Documenter.jl <documenter@juliadocs.github.io>
2022-03-21 16:14:20 +0000

    build based on 3b028ba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TFT.jl","page":"Home","title":"TFT.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TFT.jl hi ho he","category":"page"}]
+}
